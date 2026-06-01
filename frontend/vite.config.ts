6898256b61121b99bc77ebef607c51c21a8b0467
Asif Hossain <asifiscoding@gmail.com>
2026-06-01 21:20:30 +0600

    deploment required url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['spendsense-frontend-sg80.onrender.com'],
+  },
 });
