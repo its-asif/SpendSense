b14218a664bdc672915bea40f4b8f46c7006da05
Asif Hossain <asifiscoding@gmail.com>
2026-06-01 21:21:52 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: ['spendsense-frontend-sg80.onrender.com'],
+    },
 });
